fc7b3089db7e7e611e7efe33444673dfe321f68f
Corwyn <corwynnielsen@gmail.com>
2022-12-23 22:45:01 -0500

    added some comments, formatted files with black, cleaned up main.py, now loading Music cog as extension

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import logging
+import logging.handlers
+import asyncio
+import os
 from discord.ext import commands
+import discord
 
-import config_util
 from cogs.music import Music
 
-logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(
-    filename='discord-lib.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter(
-    '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
-                   description='A bot representing NYS\'s late Governor Andy Cuomo')
+async def main():
+    # Discord bot logging configuration
+    # See more here: https://docs.python.org/3/library/logging.html
+    logger = logging.getLogger("discord")
+    logger.setLevel(logging.DEBUG)
+    handler = logging.handlers.RotatingFileHandler(
+        filename="discord-lib.log",
+        encoding="utf-8",
+        maxBytes=32 * 1024 * 1024,  # 32 MiB
+        backupCount=5,
+    )
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+    )
+    logger.addHandler(handler)
+
+    # Discord bot intents
+    # See more here: https://discordpy.readthedocs.io/en/stable/api.html?highlight=intents#intents
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    bot = commands.Bot(
+        intents=intents,
+        command_prefix=commands.when_mentioned_or("!"),
+        description="A bot representing NYS's late Governor Andy Cuomo",
+    )
+
+    await bot.load_extension("cogs.music")
+    await bot.start(os.getenv("DISCORD_TOKEN", ""))
 
 
-@bot.event
-async def on_ready():
-    print('Logged in as {0} ({0.id})'.format(bot.user))
-    print('------')
-
-bot.add_cog(Music(bot))
-bot.run(config_util.get('Discord')['token'])
+asyncio.run(main())
